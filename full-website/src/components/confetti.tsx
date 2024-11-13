2262e78ec361825ac4a3cff76bdad6f4762ae90e
tofaramususa <jtofaramususa@gmail.com>
2024-11-13 17:52:52 +0400

    confetti

--- full-website/src/components/confetti.tsx
+++ full-website/src/components/confetti.tsx
@@ -1,31 +1,30 @@
 import confetti from "canvas-confetti";
 
 export default function confettiFunc() {
-	const end = Date.now() + 3 * 1000; // 3 seconds
-    const colors = ["#a786ff", "#fd8bbc", "#eca184", "#f8deb1"];
+    const duration = 5 * 1000;
+    const animationEnd = Date.now() + duration;
+    const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
  
-    const frame = () => {
-      if (Date.now() > end) return;
+    const randomInRange = (min: number, max: number) =>
+      Math.random() * (max - min) + min;
  
+    const interval = window.setInterval(() => {
+      const timeLeft = animationEnd - Date.now();
+ 
+      if (timeLeft <= 0) {
+        return clearInterval(interval);
+      }
+ 
+      const particleCount = 50 * (timeLeft / duration);
       confetti({
-        particleCount: 2,
-        angle: 60,
-        spread: 55,
-        startVelocity: 60,
-        origin: { x: 0, y: 0.5 },
-        colors: colors,
+        ...defaults,
+        particleCount,
+        origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
       });
       confetti({
-        particleCount: 2,
-        angle: 120,
-        spread: 55,
-        startVelocity: 60,
-        origin: { x: 1, y: 0.5 },
-        colors: colors,
+        ...defaults,
+        particleCount,
+        origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
       });
- 
-      requestAnimationFrame(frame);
-    };
- 
-    frame();
+    }, 250);
   };

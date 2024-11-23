8900c78f814eec85589a91da0500fe044b5a4809
Tofara Mususa <66259401+tofaramususa@users.noreply.github.com>
2024-11-23 14:17:44 +0000

    v2

--- mobile/app/(tabs)/_layout.tsx
+++ mobile/app/(tabs)/_layout.tsx
@@ -7,12 +7,24 @@ import { IconSymbol } from '@/components/ui/IconSymbol';
 import TabBarBackground from '@/components/ui/TabBarBackground';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <Tabs>
+    <Tabs 
+    screenOptions={{
+      tabBarStyle: {
+        display: 'none',
+      },
+    }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
